2df6ab9a6a77a75e4d17153258ae9026f11d7f54
Pragya <pragn@flemingcollege.ca>
2023-06-21 23:35:03 -0400

    pastebin_api
    pastebin updated

---
+++ pastebin_api.py
@@ -0,0 +1,50 @@
+'''
+Library for interacting with the PasteBin API
+https://pastebin.com/doc_api
+
+'''
+import requests
+
+PASTEBIN_API_POST_URL = 'https://pastebin.com/api/api_post.php'
+API_DEV_KEY = 'HprzAVXOBs5OW-2g0sGSOtr5hr2y1hY3'
+
+def post_new_paste(title, body_text, expiration='N', listed=True):
+    """Posts a new paste to PasteBin
+
+    Args:
+        title (str): Paste title
+        body_text (str): Paste body text
+        expiration (str): Expiration date of paste (N = never, 10M = minutes, 1H, 1D, 1W, 2W, 1M, 6M, 1Y)
+        listed (bool): Whether paste is publicly listed (True) or not (False) 
+    
+    Returns:
+        str: URL of new paste, if successful. Otherwise None.
+    """    
+    # Setup the POST message param
+    params = { 
+        "api_dev_key": API_DEV_KEY,
+        "api_option": "paste",
+        "api_paste_code": body_text,
+        'api_paste_name': title,
+        'api_paste_private': 0 if listed else 1,
+        'api_paste_expire_data': expiration
+      
+    }
+    #Send the POST request and get the response 
+    resp_msg = requests.post (PASTEBIN_API_POST_URL, data=params)
+
+    #Check whether the request was successful 
+    if resp_msg.status_code == requests.codes.ok:
+        pass
+    else:
+        print(f'Request failed.')
+        print(f'Status code: {resp_msg.status_code} ({resp_msg.reason})')
+
+    
+    return
+
+def main():
+    post_new_paste('Awesome Paste', 'This paste is not useful.\n delete whenever')
+
+if __name__== '__main__':
+    main()
